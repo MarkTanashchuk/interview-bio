86689b078d3473f6f88a9fbe0511c977b9adbb43
MarkTanashchuk <mark.tanashchuk@gmail.com>
2024-06-07 10:24:03 +0300

    chore(1): remove unused files comments and convert styles to tailwind

--- apps/1/src/routes/+page.svelte
+++ apps/1/src/routes/+page.svelte
@@ -5,43 +5,15 @@
 	let tabNodes: HTMLLIElement[] = [];
 	let tabIdxActive = 0;
 
-	function animateBorder(containerNode: HTMLUListElement, index: number): ActionReturn<number> {
+	function animateBorder(_containerNode: HTMLUListElement, index: number): ActionReturn<number> {
 		const animationCurrent = gsap.timeline();
-		const tabNodesAmount = tabNodes.length;
 		let indexCurrent = index;
-
-		tabNodes.forEach((node, i) => {
-			node.addEventListener('click', () => {
-				// check if clicked target is new and if the timeline is currently active
-				if (index != tabIdxActive) {
-					// if (animationCurrent.isActive()) {
-					// 	animationCurrent.progress(1);
-					// }
-					// indexCurrent = tabIdxActive;
-					// tabIdxActive = index;
-					// // animate bubble border to clicked target
-					// animationCurrent.to('.border', {
-					// 	x: tabNodes[tabIdxActive].offsetLeft
-					// });
-				}
-			});
-		});
-
-		// set initial article and position bubble border on first tab
-		// gsap.set('.border', { x: tabNodes[index].offsetLeft });
 
 		return {
 			update: (indexNew) => {
-				// check if clicked target is new and if the timeline is currently active
-				if (indexCurrent != indexNew) {
-					console.info(indexCurrent, indexNew);
+				if (indexCurrent !== indexNew) {
 					indexCurrent = indexNew;
-					console.info(indexCurrent, indexNew);
-					// if (animationCurrent.isActive()) {
-					// 	animationCurrent.progress(1);
-					// }
 
-					// // animate bubble border to clicked target
 					animationCurrent.to('#border', {
 						xPercent: indexCurrent * 300
 					});
@@ -53,14 +25,14 @@
 </script>
 
 <div class="flex flex-col w-full h-full items-center justify-center">
-	<h1 class="w-fit text-white font-bold text-3xl pb-[var(--n)]" style:--n="40px">1</h1>
+	<h1 class="w-fit text-white font-bold text-3xl pb-10">1</h1>
 
 	<ul class="relative flex w-full max-w-xl" use:animateBorder={tabIdxActive}>
 		{#each ['Animation', 'Branding', 'Illustration'] as text, i}
 			<!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
 			<!-- svelte-ignore a11y-click-events-have-key-events -->
 			<li
-				class="text-white font-bold text-xl"
+				class="m-0 p-2 z-10 text-white font-bold text-xl text-center cursor-pointer list-none transition-opacity"
 				style:opacity={tabIdxActive === i ? 1 : 0.5}
 				bind:this={tabNodes[i]}
 				on:click={() => (tabIdxActive = i)}
@@ -79,23 +51,6 @@
 		width: 100%;
 		font-family: 'Roboto', sans-serif;
 		background: rgb(12, 0, 28);
-		background:
-        /* linear-gradient(180deg, rgba(12, 0, 28, 1) 0%, rgba(24, 0, 60, 1) 100%) */ radial-gradient(
-			circle at bottom,
-			#1c0047 0%,
-			#0b001d 75%
-		);
-	}
-
-	li {
-		text-align: center;
-		margin: 0;
-		padding: 10px;
-		flex: auto;
-		position: relative;
-		cursor: pointer;
-		list-style: none;
-		z-index: 10;
-		transition: opacity 200ms;
+		background: radial-gradient(circle at bottom, #1c0047 0%, #0b001d 75%);
 	}
 </style>
