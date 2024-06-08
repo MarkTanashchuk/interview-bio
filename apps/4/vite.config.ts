93461bc85ed3a3d20bcc34fedde60366e7076b62
MarkTanashchuk <mark.tanashchuk@gmail.com>
2024-06-08 20:33:21 +0300

    fix(4): change port to 0.0.0.0

--- apps/4/vite.config.ts
+++ apps/4/vite.config.ts
@@ -2,13 +2,19 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-  plugins: [sveltekit()],
-  test: {
-    include: ['src/**/*.{test,spec}.{js,ts}']
-  },
-  build: {
-    commonjsOptions: {
-      include: [/@repo-ui/, /node_modules/],
-    },
-  },
+	plugins: [sveltekit()],
+	server: {
+		host: '0.0.0.0'
+	},
+	preview: {
+		host: '0.0.0.0'
+	},
+	test: {
+		include: ['src/**/*.{test,spec}.{js,ts}']
+	},
+	build: {
+		commonjsOptions: {
+			include: [/@repo-ui/, /node_modules/]
+		}
+	}
 });
